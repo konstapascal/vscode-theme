8e19b9dbe49b2f23fe77c5314b6690d17fac5eef
konstapascal <pascalkonstantinos@gmail.com>
2021-07-03 22:53:19 +0200

    initial commit

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 const os = require('os');
 
-console.log(os.platform());
+const userOS = os.platform();
+
+let vscodePath;
+if (userOS === 'win32') vscodePath = 'Win';
+if (userOS === 'linux') vscodePath = 'Linux';
+if (userOS === 'darwin') vscodePath = 'Mac';
+
+console.log(userOs, vscodePath);
