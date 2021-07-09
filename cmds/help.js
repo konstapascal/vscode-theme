75513372a2c038d6d201a52a761d41802d214f88
konstapascal <pascalkonstantinos@gmail.com>
2021-07-09 21:39:59 +0200

    changes

--- cmds/help.js
+++ cmds/help.js
@@ -1,5 +1,20 @@
+const chalk = require('chalk');
+
 function help() {
-	console.log('Help output to be written!');
+	console.log(`
+	${chalk.greenBright('vsc-theme [command] <options>')}
+
+	  ${chalk.blueBright('list')} ................ list of all available themes
+	  ${chalk.blueBright('set')} ................. set a new theme to vscode
+	  ${chalk.blueBright('add')}.................. add a new theme to the list of available ones
+	  ${chalk.blueBright('remove')} .............. remove a theme from the list of the available ones
+	  
+	  ${chalk.blueBright('help')} ................ show help menu for all commands
+	  ${chalk.blueBright('version')} ............. show the installed version of vsc-theme
+
+	  
+	  ${chalk.redBright.bold('PS: Te iubesc muuult, muult, Katy! <3')}
+	`);
 }
 
 module.exports = help;
